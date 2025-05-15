171b18264093aeef636c4f261ca29d54b17106ab
SapnaShree12 <sapnashree01@gmail.com>
2025-05-15 08:50:24 +0530

    feat:subject,behaviour subject

--- src/app/rxjs-concepts/rxjs-concepts.component.ts
+++ src/app/rxjs-concepts/rxjs-concepts.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs-concepts',
@@ -10,7 +10,11 @@ import { Observable } from 'rxjs';
 export class RxjsConceptsComponent {
 
   ngOnInit(): void {
-    this.observableData();
+    //this.observableData();
+    // this.promiseData();
+    // this.subjectFunction();
+   // this.replaySubject();
+     this.behaviourSubject();
   }
   observableData(){
 let observable =new Observable<any>(Observer=>{
@@ -27,7 +31,50 @@ observable.subscribe({
   })
   }
   promiseData(){
-
+let promise = new Promise((resolve,reject)=>{
+  resolve('sapna');
+  //reject('shrees');
+});
+promise.then(data=>console.log(data))
+.catch(err=>console.log(err))
   }
 
+  //subjec
+  subjectFunction() {
+    let subject = new Subject();
+
+    console.log("Before subscribe - emitting 'suki'");
+    subject.next('subject-suki');
+
+    subject.subscribe({
+      next: data => console.log("Subscriber received:", data)
+    });
+
+    console.log("after subscri");
+    subject.next('subject-sapna');
+    subject.next('subject-black');
+  }
+
+  replaySubject(){
+    let replaySubject = new ReplaySubject(-1);
+    replaySubject.next('suki');
+    replaySubject.next('sapna');
+    replaySubject.next('black');
+    replaySubject.subscribe({
+      next: data => console.log("Subscriber received:", data)
+    });
+    replaySubject.next('suki');
+
+  }
+  behaviourSubject(){
+    let behaviour = new BehaviorSubject('suki')
+    behaviour.next('suki');
+    behaviour.next('sapna');
+    behaviour.next('black');
+    behaviour.subscribe({
+      next: data => console.log("Subscriber received:", data)
+    });
+    behaviour.next('suki');
+
+  }
 }
